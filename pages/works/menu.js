4d72eca7edd1995cfa4167366f80191cefa71db6
nozloy <31240637+nozloy@users.noreply.github.com>
2022-05-11 01:53:01 +0300

    grammar fixes

--- pages/works/menu.js
+++ pages/works/menu.js
@@ -1,4 +1,4 @@
-import { Container, Badge, Box } from '@chakra-ui/react'
+import { Container, Box } from '@chakra-ui/react'
 import { Title, WorkImage, Meta } from '../../components/work'
 import P from '../../components/paragraph'
 import Layout from '../../components/layouts/article'
@@ -7,17 +7,17 @@ const Work = () => (
     <Layout title="Меню">
         <Container>
             <Title>
-                Термокружки <Badge>с гравировкой</Badge>
+                Меню
             </Title>
             <P>
                 Обложка с гравировкой логотипа на лицевой стороне.
                 Обработка составом, включающим масло и воск, что дало водоотталкивающие свойства и приятный цвет.
                 Печатный блок на синтетической бумаге, которой также не страшна вода и масло.
-                Крепеление на кольцевом механизме - это позволяет в дальнейшем без проблем менять информацию в меню.
+                Крепление на кольцевом механизме - это позволяет в дальнейшем без проблем менять информацию в меню.
             </P>
             <Box mt={4}>
                 <Meta>Кстати</Meta>
-                <span>Посетителей кафе и ресторанов чаще всего привлекает опрятное и неизносившееся меню. </span>
+                <span>Посетителей кафе и ресторанов чаще всего привлекает опрятное и не износившееся меню. </span>
             </Box>
             <WorkImage src="/images/works/menu.jpg" alt="Меню" />
             <WorkImage src="/images/works/menu_2.jpg" alt="Меню" />
